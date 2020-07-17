7a86e4eb0ff86f4533f39322dfad0f14adf0ec48
engrbugs <engrbugs@gmail.com>
2020-07-17 14:40:55 -0700

    2020-07-17-1440

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function SetTimer() {
+function SetTimer(props) {
   return (
-    <div className="time-container">
-      <h1>Break Time</h1>
-      <div>
-        <button>
+    <div className="timer-container">
+      <h2>{props.title}</h2>
+      <div className="flex actions-wrapper">
+        <button onClick={props.handleDecrease}>
           <i className="fas fa-minus" />
         </button>
-        <span>5</span>
-        <button>
+        <span>{props.count}</span>
+        <button onClick={props.handleDecrease}>
           <i className="fas fa-plus" />
         </button>
       </div>
@@ -20,9 +20,43 @@ function SetTimer() {
 }
 
 function App() {
+  const [breakCount, setBreakCount] = useState(5);
+  const [sessionCount, setSessionCount] = useState(25);
+  const breakProps = {
+    title: "Break Length",
+    count: breakCount,
+    // handleDecrease: this.handleBreakDecrease,
+    // handleIncrease: this.handleBreakIncrease,
+  };
+  const sessionProps = {
+    title: "Session Length",
+    count: sessionCount,
+    // handleDecrease: this.handleSessionDecrease,
+    // handleIncrease: this.handleSessionIncrease,
+  };
   return (
-    <div className="App">
-      <div><SetTimer /></div>
+    <div>
+      <div className="flex">
+        <SetTimer {...breakProps} />
+        <SetTimer {...sessionProps} />
+      </div>
+      <div className="clock-container">
+        <h1>Session</h1>
+        <span>25:00</span>
+        <div className="flex">
+          <button 
+          // onClick={this.handlePlayPause}
+          >
+            <i className="fas fa-play" />
+          </button>
+          <button 
+          // onClick={this.handleReset}
+          >
+            <i className="fas fa-sync" />
+          </button>
+        </div>
+
+      </div>
     </div>
   );
 }
