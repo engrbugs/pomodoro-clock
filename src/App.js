45f5d4fd1c5a73df0def4d36955260c6c9e093ec
engrbugs <engrbugs@gmail.com>
2020-07-17 13:40:33 -0700

    2020-07-17-1340

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function SetTimer() {
+  return (
+    <div className="time-container">
+      <h1>Break Time</h1>
+      <div>
+        <button>
+          <i className="fas fa-minus" />
+        </button>
+        <span>5</span>
+        <button>
+          <i className="fas fa-plus" />
+        </button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div><SetTimer /></div>
     </div>
   );
 }
